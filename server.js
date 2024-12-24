f2e1c9e1ed9c7392dd9494bc67fc144d867298b9
Rohit Bisht <124440810+rohitbisht-coder@users.noreply.github.com>
2024-12-24 22:06:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 import { app } from "./app.js";
 import dotenv from "dotenv"
-import https from "https"
-import fs from "fs"
 import { connectToDB } from "./src/config/db.js";
 dotenv.config()
-const option = {
-    key:fs.readFileSync("Certificates/private.key"),
-    cert:fs.readFileSync("Certificates/server.crt")
-}
-https.createServer(option, app).listen(process.env.PORT  , async()=>{
+
+app.listen(process.env.PORT  , async()=>{
     console.log("server  is listening  on " + process.env.PORT)
     await connectToDB()
 })
